24f2958c96da76d9c901a7d413878807d1a40b9e
Smellyslab <ilike2code1234@gmail.com>
2022-06-18 23:17:52 -0400

    added new mining method for increased speed and functionality

--- bin/smellycoin.js
+++ bin/smellycoin.js
@@ -9,11 +9,15 @@ const argv = require('yargs')
     .describe('p', 'HTTP port. (3001 by default)')
     .alias('l', 'log-level')
     .describe('l', 'Log level (7=dir, debug, time and trace; 6=log and info; 4=warn; 3=error, assert; 6 by default).')
-    .describe('peers', 'Peers list.')
     .describe('name', 'Node name/identifier.')
     .array('peers')
+    .describe('peers', 'Peers list.')
+    .alias('mng', 'mining')
+    .describe('mng', 'enable minign "True", or "False"; True by default ')
+    .alias('addr', 'address')
+    .describe('address', 'address to use as minerconfig default \'miner\' (change to your address to receive rewards)')
     .help('h')
     .alias('h', 'help')
     .argv;
 
-naivecoin(argv.host, argv.port, argv.peers, argv.logLevel, argv.name);
+naivecoin(argv.host, argv.port, argv.peers, argv.logLevel, argv.name, argv.mining, argv.address);
